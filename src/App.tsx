d7c088ecfee75ac6698edd01b6b75ad2948b6191
naveen.singh <naveen.singh@itelasoft.com>
2025-04-22 12:14:31 +0530

    add db connection with mongo db cluster

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-3xl font-bold text-center">M32 Assignment</h1>;
+  return <h1 className="text-3xl font-bold text-center">GradeGenie</h1>;
 }
 
 export default App;
